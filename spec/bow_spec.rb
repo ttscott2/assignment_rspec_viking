85421de9e4b5862b99e4cdd32c42e02fd3edf7b9
ttscott2 <timothytscott3@gmail.com>
2016-11-19 20:22:05 -0600

    finish bow spec

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,33 @@
 # Your code here
+require 'weapons/bow'
+
+describe Bow do
+  let(:bow) { Bow.new }
+
+  describe '#arrows' do
+    it 'is readable' do
+      expect{ bow.arrows }.to_not raise_error
+    end
+
+    it 'starts with default number of arrows' do
+      expect(bow.arrows).to eq(10)
+    end
+
+    it 'starts with the number of arrows passed in' do
+      big_bow = Bow.new(20)
+      expect(big_bow.arrows).to eq(20)
+    end
+  end
+
+  describe '#use' do
+    it 'reduces arrows by 1' do
+      bow.use
+      expect(bow.arrows).to eq(9)
+    end
+
+    it 'throws an error if no arrows' do
+      10.times { bow.use }
+      expect { bow.use }.to raise_error('Out of arrows')
+    end
+  end
+end
